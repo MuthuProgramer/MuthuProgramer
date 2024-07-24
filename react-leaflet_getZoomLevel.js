611c3e9a62d7c34c5026316193fe257042024760
MuthuProgramer <86128464+MuthuProgramer@users.noreply.github.com>
2024-07-25 01:06:21 +0530

    Update react-leaflet_getZoomLevel.js

--- react-leaflet_getZoomLevel.js
+++ react-leaflet_getZoomLevel.js
@@ -1,3 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Polyline, Tooltip, useMapEvents, useMap,ZoomControl, Circle  } from 'react-leaflet';
+
+import L from 'leaflet';
+
 const MapEventHandler = ({ setZoomLevel,setIconSize  }) => {
   useMapEvents({
       zoomend: (event) => {
@@ -19,3 +24,52 @@ const getMarkerSize = (zoom) => {
 console.log("10, 50 - zoom * 2",Math.max(10, zoom * 4))
 return Math.max(8, zoom * 4); // Minimum size 10, maximum size 50
 };
+
+  const Fetch_GeoJson_Data = () => {
+const [zoomLevel, setZoomLevel] = useState(10); // Initial zoom level
+
+  const [iconSize, setIconSize] = useState([50, 50]); // Initial icon size
+
+     // Create a custom icon with the dynamic size
+     const customIcon = new L.Icon({
+      iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png', // Change to your marker URL
+      iconSize: iconSize,
+      iconAnchor: [iconSize[0] / 2, iconSize[1]], // Anchor the icon to its center
+      popupAnchor: [0, -iconSize[1] / 2], // Position the popup above the marker
+  });
+
+useEffect(()=>{
+  console.log("zoomLevel",zoomLevel);
+},[zoomLevel])
+  return (
+    <>
+  <MapContainer
+       center={[12.2, 80.27]} 
+       zoom={zoomLevel} 
+       zoomControl={false}
+       style={{ height: '100vh', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+         <ZoomControl position="bottomright" />
+
+          <Marker 
+          position={[12.2, 80.27]} 
+          icon={customIcon} >
+                <Tooltip
+                  
+                >
+                  hi wellcome
+                </Tooltip>
+            </Marker>
+
+         <MapEventHandler setZoomLevel={setZoomLevel} setIconSize={setIconSize}/>
+         <Circle
+                center={[12.2, 80.27]}
+                radius={250000} // Radius in meters (250 km)
+                color="blue"
+            />
+            </MapContainer>
+            )
+            };
+            export default Fetch_GeoJson_Data;
